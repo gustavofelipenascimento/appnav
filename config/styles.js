63a68f9db14ce9e7f1c81453226d70215fd2f6e3
Gustavo Felipe do Nascimento <gustavo.nascimento1@alunos.sc.senac.br>
2024-02-29 16:46:24 -0300

    inicial

---
+++ config/styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+/**
+ * Vá até o final da palavra contendo o componente
+ * que deseja importar e aperte CTRL + Espaço
+ * neste caso
+ */
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    titulo1: {
+        fontSize: 18,
+        fontWeight: "bold",
+    },
+});
+
+export default styles;
